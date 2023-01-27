4606625354b3c9813d226b5354c6248c82989f61
Adomas Baliuka <A.Baliuka@physik.uni-muenchen.de>
2023-01-27 14:54:38 +0100

    Demo of RNG for freely chosen period. Not fully tested.

--- example/example.cpp
+++ example/example.cpp
@@ -1,10 +1,63 @@
-#include <rlcg.hpp>
-
 #include <iostream>
 #include <cassert>
 #include <vector>
 
+#include "rlcg.hpp"
+
+/// Goal: get LCG that has ANY DESIRED PERIOD.
+void test_fixed_period_lcg() {
+    constexpr auto M = 20; // period of lcg. Can choose arbitrarily.
+
+    // To get period = M, choose C to be relatively prime to M...
+    constexpr auto C = 13;
+    // ... and A s.t. (1.) (A-1) is divisible by all prime factors of M and (2.) (A-1) is divisible by 4 if M is.
+    constexpr auto A = (2*2*5) + 1;
+
+    constexpr auto seed = 42; // Seed of lcg. Can choose arbitrarily.
+    rlcg::details::ReversibleLCG<M, A, C, 0> rng(seed);
+
+    const int numtests = 5 * M;
+
+    std::vector<unsigned int> forward;
+    std::cout << "\nForward:\n";
+    for(int i = 0; i<numtests; ++i){
+        forward.push_back(rng.next());
+        std::cout << forward.back() << std::endl;
+        assert(forward.back() <= rng.max());
+    }
+
+    // test periodicity. Note: test only works correctly if forward.size() divides the period!
+    for(std::size_t i = 0; i < forward.size(); ++i){
+        assert(forward[i] == forward[(i + M) % forward.size()]);
+    }
+
+    std::cout << "\nBackwards:\n";
+    for(int i = numtests - 2; i>=0; --i){
+        unsigned int val = rng.prev();
+        std::cout << val << std::endl;
+        assert(val == forward[static_cast<unsigned int>(i)]);
+    }
+
+    std::vector<unsigned int> backward;
+    std::cout << "\nBackwards:\n";
+    for(int i = 0; i<numtests; ++i){
+        backward.push_back(rng.prev());
+        std::cout << backward.back() << std::endl;
+    }
+    std::cout << "\nForwards:\n";
+    for(int i = numtests - 2; i>=0; --i){
+        unsigned int val = rng.next();
+        std::cout << val << std::endl;
+        assert(val == backward[static_cast<unsigned int>(i)]);
+    }
+}
+
+
 int main() {
+    test_fixed_period_lcg();
+    std::cout << "\n================== Original Example ==================\n";
+    return 0;
+
 	rlcg::ReversibleLCG rng(42);
 	const int numtests = 10000;
 
--- include/rlcg.hpp
+++ include/rlcg.hpp
@@ -63,6 +63,7 @@ constexpr std::tuple<uint64_t, uint64_t> extendedEuclid(uint64_t a, uint64_t b) 
     }
 }
 
+
 template <typename T>
 constexpr inline T positive_modulo(T i, T n) {
     return (i % n + n) % n;
@@ -71,24 +72,33 @@ constexpr inline T positive_modulo(T i, T n) {
 //modulus M, multiplicand A, increment C, least significant bits to discard D
 template<uint64_t M = 1ull<<63ul, uint64_t A = 6364136223846793005, uint64_t C = 1442695040888963407, uint64_t D = 32>
 class ReversibleLCG {
-    static_assert(isPowerOfTwo(M), "M is not a power of two as it should be");
+     static_assert(isPowerOfTwo(M) || D == 0,
+             "M is not a power of two as it should be. I only tested behaviour for non-prime M when D = 0.");
     uint64_t x;
 public:
     explicit ReversibleLCG(unsigned int seed) : x(seed){}
-
     // Hi 32-bits is the initial value, lo 32-bit is a random seed
     void set_state(uint64_t s) { x = s; }
 
     unsigned int next() {
-        //nextx = (a * x + c) % m;
-        x = (A * x + C) & (M - 1);
+        if constexpr (isPowerOfTwo(M)) {
+            x = (A * x + C) & (M - 1);
+        } else {
+            x = (A * x + C) % M;
+        }
         return x >> D;
     }
 
     unsigned int prev() {
         constexpr uint64_t ainverse = std::get<0>(extendedEuclid(A, M));
-        //prevx = (ainverse * (x - c)) mod m
-        x = ainverse * (x - C) & (M - 1);
+        if constexpr (isPowerOfTwo(M)) {
+            x = ainverse * (x - C) & (M - 1);
+        } else {
+            constexpr auto minus_C_modM = positive_modulo(
+                    -static_cast<long long>(C) , static_cast<long long>(M));
+            x = ainverse * (x + minus_C_modM) % M;
+        }
+
         return x >> D;
     }
 
@@ -97,7 +107,6 @@ public:
         return (M - 1) >> D;
     }
 };
-
 } // end namespace details
 
 using ReversibleLCG = details::ReversibleLCG<>;
